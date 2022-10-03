cf8aefab65aa1ca3e2ca691b3ed74edf03545fd8
admin <admin@VTORAYA0>
2022-10-03 20:40:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SquareApp
+{   
+    internal class Program
+    {
+        static int Square(int number) => number * number;
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int result = Square(number);
+            Console.WriteLine($"{number} * {number} = {result}");
+        }
+    }
+}
